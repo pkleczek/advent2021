0c5c23850ed0b0d783ab4a43b13b871c5cc4ff9c
Paweł Kłeczek <pkleczek@gmail.com>
2021-12-02 08:59:40 +0100

    Refactor Day02

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,39 +1,45 @@
 package day02
 
-fun part1() {
-    data class Position(var horizontal: Int = 0, var depth: Int = 0)
+data class Position(var horizontal: Int = 0, var depth: Int = 0, var aim: Int = 0)
+enum class Direction { FORWARD, UP, DOWN }
 
+data class Command (val direction: Direction, val distance: Int) {
+    companion object {
+        fun parse(input: String): Command {
+            val (command, distance) = input.split(' ')
+            return Command(Direction.valueOf(command.uppercase()), distance.toInt())
+        }
+    }
+}
+
+fun part1() {
     val position = generateSequence(::readlnOrNull)
-        .map { it.split(' ') }
-        .map { it[0] to it[1].toInt() }
-        .fold(Position()) { acc, pair ->
-            when (pair.first) {
-                "forward" -> Position(acc.horizontal + pair.second, acc.depth)
-                "down" -> Position(acc.horizontal, acc.depth + pair.second)
-                "up" -> Position(acc.horizontal, acc.depth - pair.second)
-                else -> acc
+        .map { Command.parse(it) }
+        .fold(Position()) { acc, command ->
+            when (command.direction) {
+                Direction.FORWARD -> Position(acc.horizontal + command.distance, acc.depth)
+                Direction.DOWN -> Position(acc.horizontal, acc.depth + command.distance)
+                Direction.UP -> Position(acc.horizontal, acc.depth - command.distance)
             }
         }
     println(position.horizontal * position.depth)
-
 }
 
 fun part2() {
-    data class Position(var horizontal: Int = 0, var depth: Int = 0, var aim: Int = 0)
-
     val position = generateSequence(::readlnOrNull)
-        .map { it.split(' ') }
-        .map { it[0] to it[1].toInt() }
+        .map { Command.parse(it) }
         .fold(Position()) { acc, pair ->
-            when (pair.first) {
-                "forward" -> Position(acc.horizontal + pair.second, acc.depth + (acc.aim * pair.second), acc.aim)
-                "down" -> Position(acc.horizontal, acc.depth, acc.aim + pair.second)
-                "up" -> Position(acc.horizontal, acc.depth, acc.aim - pair.second)
-                else -> acc
+            when (pair.direction) {
+                Direction.FORWARD -> Position(
+                    acc.horizontal + pair.distance,
+                    acc.depth + (acc.aim * pair.distance),
+                    acc.aim
+                )
+                Direction.DOWN -> Position(acc.horizontal, acc.depth, acc.aim + pair.distance)
+                Direction.UP -> Position(acc.horizontal, acc.depth, acc.aim - pair.distance)
             }
         }
     println(position.horizontal * position.depth)
-
 }
 
 fun main() {
