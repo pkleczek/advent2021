6c8fbc98437a0c0dec5700059f3f88f5324ca412
Paweł Kłeczek <pkleczek@gmail.com>
2021-12-28 16:34:43 +0100

    Unfinished recursive

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,5 +1,7 @@
 package day14
 
+import java.util.*
+
 data class Spec(val initial: String, val transitions: Map<String, String>)
 
 fun part1(input: Spec) {
@@ -27,7 +29,52 @@ fun part1(input: Spec) {
 
 }
 
-fun part2(input: List<String>) {
+fun part2(input: Spec) {
+    fun expand(input: LinkedList<Char>, mapping: Map<String, String>) {
+        val iterator = input.listIterator()
+        var previous: Char? = null
+        while (iterator.hasNext()) {
+            val current = iterator.next()
+            if (previous != null) {
+                val mapped = mapping["$previous$current"]
+                if (mapped != null) {
+                    iterator.set(mapped[0])
+                    iterator.add(current)
+                }
+
+            }
+            previous = current
+        }
+    }
+
+    fun transform(input: LinkedList<Char>, mapping: Map<String, String>, counter: Int): Map<Char, Int> {
+        if (counter == 0)
+            return input.groupingBy { it }.eachCount()
+
+        expand(input, mapping)
+
+        return input
+            .windowed(2)
+            .map {
+                transform(LinkedList(it), mapping, counter - 1)
+            }
+            .fold((mapOf())) { a, v -> (a.keys + v.keys).associateWith { (a[it] ?: 0) + (v[it] ?: 0) } }
+    }
+
+    val transform = input.initial.windowed(2).map {
+        transform(LinkedList(it.toList()), input.transitions, 10)
+    }
+        .fold((mapOf<Char, Int>())) { a, v ->
+            (a.keys + v.keys).associateWith {
+                (a[it] ?: 0) + (v[it] ?: 0)
+            }
+        }
+
+    println(transform)
+    val max = transform.maxOf { it.value }
+    val min = transform.minOf { it.value }
+    println(max - min)
+
     // TODO: figure out something clever for part 2, brute force takes too much mem
 }
 
@@ -42,5 +89,5 @@ fun main() {
 
     val spec = Spec(initial, transitions)
     println(spec)
-    part1(spec)
+    part2(spec)
 }
